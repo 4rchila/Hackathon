a2d6e8d5db70ed6c14b257d14eb0c3f6b6fc0a4f
German Archila <106097211+4rchila@users.noreply.github.com>
2024-10-29 23:00:42 -0600

    ultimos cambios en funcionalidad
    cambios en funcionalidad

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 100) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
 
-    window.addEventListener("scroll", function() {
-        const navbar = document.querySelector(".navbar");
-        if (window.scrollY > 100) { // Cambia 100 por la cantidad de píxeles deseada
-            navbar.classList.add("scrolled");
-        } else {
-            navbar.classList.remove("scrolled");
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    const testButtons = document.querySelectorAll('.test-btn');
+    testButtons[0].addEventListener('click', function() {
+        window.location.href = 'https://www.16personalities.com/es/test-de-personalidad';
     });
+});
 
+document.addEventListener("DOMContentLoaded", function() {
+    const testButtons = document.querySelectorAll('.btn');
+    testButtons[0].addEventListener('click', function() {
+        window.location.href = 'https://www.16personalities.com/es/test-de-personalidad';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const testButtons = document.querySelectorAll('.cta-btn');
+    testButtons[0].addEventListener('click', function() {
+        window.location.href = 'https://www.16personalities.com/es/test-de-personalidad';
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const testButtons = document.querySelectorAll('.cta-btn');
+    testButtons[0].addEventListener('click', function() {
+        window.location.href = 'https://chat.openai.com/?test-de-gustos-habilidades';
+    });
+});
